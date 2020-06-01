a34c7027612429f7af4f56f8fa533fb281531f7b
Mouhayad I. HOUNKANRIN A <h.ayinlamouhayad@gmail.com>
2020-06-01 08:32:44 +0000

    ajout de la validaion d'un témoignage; fichier de config user admin; suppression de la maintenance; suppression de la fonctionalité de supp chap, module, formation.

---
+++ src/configuration/user.js
@@ -0,0 +1,7 @@
+export const ADMIN_PRINCIPAL = 'ADMIN_PRINCIPAL';
+export const FORMATEUR = 'FORMATEUR';
+export const MARKETEUR = 'MARKETEUR';
+
+export const LISTE_ROLES = [];
+
+export const NOTHING = '';
--- src/views/backOffice/chapitres/ListeChapitres.vue
+++ src/views/backOffice/chapitres/ListeChapitres.vue
@@ -50,7 +50,7 @@
           <v-icon dark>mdi-pencil</v-icon>
         </v-btn>
       </template>
-      <template v-slot:item.supprimer="{ item }">
+      <!-- <template v-slot:item.supprimer="{ item }">
         <v-btn
           fab
           dark
@@ -59,7 +59,7 @@
           @click="supp(item.id)">
           <v-icon dark>mdi-delete</v-icon>
         </v-btn>
-      </template>
+      </template> -->
     </v-data-table>
     <SnackComp
       :value="valueSnack"
@@ -91,7 +91,7 @@ export default {
         { text: 'Prix', value: 'price', width: 80 },
         { text: 'Status', value: 'status', width: 80 },
         { text: 'Modifier', value: 'modifier', width: 80 },
-        { text: 'Supprimer', value: 'supprimer', width: 80 },
+        // { text: 'Supprimer', value: 'supprimer', width: 80 },
       ],
       expanded: [],
       singleExpand: false,

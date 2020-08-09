af97edc54d619fc87e78a5d64386d379206e0850
Mouhayad I. HOUNKANRIN A <h.ayinlamouhayad@gmail.com>
2020-08-09 17:55:44 +0000

    nottoyage de code

--- src/components/site/home/Section3.vue
+++ src/components/site/home/Section3.vue
@@ -1,72 +1,8 @@
 <template>
   <section class="white">
-    <!-- <v-container>
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-row align="center" class="fill-height">
-            <v-col class="text-center">
-              <p class="mb-0">
-                {{page.resume}}
-              </p>
-              <v-btn
-                small
-                color="bg-blue-grad my-2"
-                :to="{ name: 'bfx-faisons' }">
-                En savoir plus
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12" sm="6" class="d-none d-sm-flex">
-          <v-row justify="center" class="fill-height">
-            <v-img src="https://image.flaticon.com/icons/png/512/32/32441.png" max-width="250"></v-img>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container> -->
     <v-container>
       <div class="barre bg-blue-grad mb-5"></div>
       <h1 class="text-center" v-animate-css="'fadeInDown'">Nos services</h1>
-      <!-- <v-row class="my-5">
-        <v-col cols="12" sm="4" >
-          <CardAction
-            titre="Initiation au trading"
-            description="Apprenez les b-a-ba du trading, comprenez comment
-              fonctionne le marché et devenez rentable en quatre semaines.
-              L’une des meilleures formations par lesquelles débuter."
-            libelle-btn="Débuter la formation"
-            :index="0"
-            :is-lock="false"
-            @action="$router.push({
-              name: 'bfx-module',
-              params: { idModule: 2 }
-            })"/>
-        </v-col>
-        <v-col cols="12" sm="4" >
-          <CardAction
-            titre="Formation Débutant"
-            description="Prédire les quatre mouvements du marché"
-            libelle-btn="Débuter la formation"
-            :index="1"
-            :is-lock="true"
-            @action="$router.push({
-              name: 'bfx-module',
-              params: { idModule: 3 }
-            })"/>
-        </v-col>
-        <v-col cols="12" sm="4" >
-          <CardAction
-            titre="Formation Avancée"
-            description="En cours"
-            libelle-btn="Débuter la formation"
-            :index="2"
-            :is-lock="true"
-            @action="$router.push({
-              name: 'bfx-module',
-              params: { idModule: 4 }
-            })"/>
-        </v-col>
-      </v-row> -->
     </v-container>
     <v-container>
       <v-row justify="space-between">
@@ -83,7 +19,6 @@
 </template>
 
 <script>
-// import CardAction from '@/components/site/general/CardAction.vue';
 import CardImg from '@/components/site/general/CardImg.vue';
 import { listeModuleByFormation } from '@/api/modules/index';
 import { listeChapitreByModule } from '@/api/chapitres/index';
@@ -91,7 +26,7 @@ import { BASE_HOST } from '@/api/config/config';
 import { mapGetters } from 'vuex';
 
 export default {
-  components: { /* CardAction, */ CardImg },
+  components: { CardImg },
   data: () => ({
     page: {
       content: '',
@@ -155,9 +90,7 @@ export default {
   async mounted() {
     await this.getData();
   },
-  destroyed() {
-    // db.ref(this.ref).off();
-  },
+  destroyed() {},
 };
 </script>
 
